7d5858464ac6d303fec746cf965795a8e3a6a4b5
p-x9 <50244599+p-x9@users.noreply.github.com>
2024-06-04 15:35:22 +0900

    Initial Commit

---
+++ Sources/weak_self_check.swift
@@ -0,0 +1,14 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+// 
+// Swift Argument Parser
+// https://swiftpackageindex.com/apple/swift-argument-parser/documentation
+
+import ArgumentParser
+
+@main
+struct weak_self_check: ParsableCommand {
+    mutating func run() throws {
+        print("Hello, world!")
+    }
+}
