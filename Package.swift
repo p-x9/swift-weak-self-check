f1cce5575db8eb78ee187b3492ca2e0131de142b
p-x9 <50244599+p-x9@users.noreply.github.com>
2024-06-04 15:37:20 +0900

    format

--- Package.swift
+++ Package.swift
@@ -1,18 +1,15 @@
-// swift-tools-version: 5.10
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version: 5.9
 
 import PackageDescription
 
 let package = Package(
-    name: "weak-self-check",
+    name: "swift-weak-self-check",
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
-            name: "weak-self-check",
+            name: "WeakSelfCheck",
             dependencies: [
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
             ]
